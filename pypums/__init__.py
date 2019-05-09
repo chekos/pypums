5e5301fb7d549a789b221734a0251c94860d3a46
chekos <chekos.data@gmail.com>
2019-05-09 16:43:01 -0700

    cleaning init file?

--- pypums/__init__.py
+++ pypums/__init__.py
@@ -16,35 +16,35 @@ import requests
 import time
 import us
 
-import pypums
-from pypums.download import download_acs_data
-from pypums.url_builder import build_acs_url
+import pypums.pypums as pypums
+# from pypums.download import download_acs_data
+# from pypums.url_builder import build_acs_url
 
 
-def get_data(
-    year: Union[int, str] = "2017",
-    survey: Union[str, int] = "1-Year",
-    person_or_household: str = "person",
-    state: str = "California",
-    download_path: str = "../data/raw/",
-    extract: bool = True,
-    extract_path: str = "../data/interim/",
-):
-    """
-    Builds URL and downloads ACS 1-Year or 5-Year state PUMS estimates into a specified folder (defaults to ../data/raw/).
-    """
+# def get_data(
+#     year: Union[int, str] = "2017",
+#     survey: Union[str, int] = "1-Year",
+#     person_or_household: str = "person",
+#     state: str = "California",
+#     download_path: str = "../data/raw/",
+#     extract: bool = True,
+#     extract_path: str = "../data/interim/",
+# ):
+#     """
+#     Builds URL and downloads ACS 1-Year or 5-Year state PUMS estimates into a specified folder (defaults to ../data/raw/).
+#     """
 
-    # builds URL
-    URL = build_acs_url(
-        year=year, survey=survey, person_or_household=person_or_household, state=state
-    )
+#     # builds URL
+#     URL = build_acs_url(
+#         year=year, survey=survey, person_or_household=person_or_household, state=state
+#     )
 
-    # download data
-    download_acs_data(
-        url=URL, download_path=download_path, extract=extract, extract_path=extract_path
-    )
+#     # download data
+#     download_acs_data(
+#         url=URL, download_path=download_path, extract=extract, extract_path=extract_path
+#     )
 
-    return None
+    # return None
 
 
 ## from realpython.com
@@ -57,4 +57,4 @@ def tree(directory):
 
 
 if __name__ == "__main__":
-    get_data()
+    pypums.get_data()
