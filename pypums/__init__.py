92a816ab7d1c9b16d430b871b7436b890edadba5
chekos <chekos.data@gmail.com>
2019-05-09 16:55:22 -0700

    adding acs class to init

--- pypums/__init__.py
+++ pypums/__init__.py
@@ -6,17 +6,9 @@ __author__ = """Sergio Sánchez Zavala"""
 __email__ = "sergio@cimarron.io"
 __version__ = "0.0.1"
 
-"""Main module."""
-# imports
-# from pathlib import Path
-# from typing import Union
-# from tqdm.auto import tqdm
-# from zipfile import ZipFile
-# import requests
-# import time
-# import us
 
 from pypums.pypums import *
+from pypums.surveys import ACS
 
 if __name__ == "__main__":
     pypums.get_data()
