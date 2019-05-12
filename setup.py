4edcfcd652dc885babe81c57c199643b24dfdb56
chekos <chekos.data@gmail.com>
2019-05-11 17:05:57 -0700

    bumpversions

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/chekos/pypums",
-    version="version='0.0.5'",
+    version="0.0.5",
     zip_safe=False,
 )
