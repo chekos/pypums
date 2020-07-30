aee339785908d357894fa38b55b2cd1add1c2168
Sergio Sanchez <ssanchez@alluma.org>
2020-07-30 09:26:01 -0700

    adding missing dirs

---
+++ pypums/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Download Public Use Micro Sample (PUMS) data files from the US Census Bureau's FTP server."""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
