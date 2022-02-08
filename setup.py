b3b6cd1db914c05f9e849275185df7201fb1a65c
Sergio Sanchez <ssanchez@alluma.org>
2022-02-08 08:26:24 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="pypums",
+    description="Download Public Use Micro Sample (PUMS) data files from the US Census Bureau's FTP server.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Sergio Sanchez",
+    url="https://github.com/chekos/pypums",
+    project_urls={
+        "Issues": "https://github.com/chekos/pypums/issues",
+        "CI": "https://github.com/chekos/pypums/actions",
+        "Changelog": "https://github.com/chekos/pypums/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["pypums"],
+    install_requires=["us"],
+    extras_require={"test": ["pytest"]},
+    python_requires=">=3.6",
+)
