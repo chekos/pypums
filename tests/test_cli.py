3d60cd375d2b768c00608ec4d81f03b3b1a8e45b
Sergio Sanchez <ssanchez@alluma.org>
2022-02-16 17:20:06 -0800

    add test for cli

---
+++ tests/test_cli.py
@@ -0,0 +1,30 @@
+from typer.testing import CliRunner
+import pytest
+
+from pypums import __app_name__, __version__, cli
+
+runner  = CliRunner()
+
+@pytest.mark.parametrize(
+    "options",
+    (
+        ["-v"],
+        ["--version"],
+    ),
+)
+def test_version(options):
+    result = CliRunner().invoke(cli.cli, options)
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}" in result.output
+
+@pytest.mark.parametrize(
+    "options",
+    (
+        ["--help"],
+    ),
+)
+def test_help(options):
+    result = CliRunner().invoke(cli.cli, options)
+    assert result.exit_code == 0
+    assert result.output.startswith("Usage: ")
+    assert "--help" in result.output
